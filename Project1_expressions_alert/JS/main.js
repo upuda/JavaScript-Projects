d8917b86821502e77f714b6dee0e9a99ff7fad98
upuda <Upanya.Udayendran@my-aolcc.com>
2024-10-20 11:26:20 -0400

    commit after adding the basic javascript assignment

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+//To display an alert
+window.alert("Hello, World!");
+
+//To write the text "Hello, World!"
+document.write("Hello, World!<br>");
+
+//To check the variable assignment, and printing the variable
+var A = "This is a string";
+document.write(A);
+
+//To assign the variable with a text, and then print that in the alert
+var welcomeMessage = "Welcome!!";
+window.alert(welcomeMessage);
+
+//To check string escape
+var msg = "Sally said, \“I don’t want to go\”?<br>";
+document.write(msg);
+document.write("<br>\"Be who you are and say what you feel," + " because those who mind don't matter and those who matter don't mind.\"" + "- Dr. Seuss");
+
+//Checking multiple variable assignment
+var film = "Titanic", hero = "Jack", heroine = "Rose";
+document.write(hero+"<br>");
+
+//expression
+document.write("Sum of the numbers  (10+5): "+ (10+5));
+
+//Concatenation of 2 variables together
+var sent1 = "<br>This is the beginning of the string";
+var sent2 = " and this is the end of the string";
+document.write(sent1+sent2);
