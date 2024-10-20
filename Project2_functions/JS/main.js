da9858e2a68ec88d94c29b888da963d16c355f31
upuda <Upanya.Udayendran@my-aolcc.com>
2024-10-20 12:54:28 -0400

    commit after adding the 2nd folder

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+//function created where clicking on the button
+function My_First_Function(){
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerText = str;
+}
+//function created where clicking on the paragraph
+function myFunction(){
+    var sentence = "I am learning";
+    sentence += " a lot from this book";
+    document.getElementById("Concatenate").innerText = sentence;
+}
+   
